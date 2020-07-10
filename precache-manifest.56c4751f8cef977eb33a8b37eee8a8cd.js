d3f93488aeec83a64229b96df14ec20fe5461ee5
NiclasSandberg <nike_pike89@hotmail.com>
2020-07-10 17:26:47 +0200

    Updates

---
+++ precache-manifest.56c4751f8cef977eb33a8b37eee8a8cd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef273cdb405f7bda5c931b48331e3f02",
+    "url": "/github.io/jonas-app.git/index.html"
+  },
+  {
+    "revision": "b70fbd64f33f3ad3335a",
+    "url": "/github.io/jonas-app.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "da5dfee3d70ebb472ed6",
+    "url": "/github.io/jonas-app.git/static/css/main.9c92a2c7.chunk.css"
+  },
+  {
+    "revision": "b70fbd64f33f3ad3335a",
+    "url": "/github.io/jonas-app.git/static/js/2.9a33c963.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github.io/jonas-app.git/static/js/2.9a33c963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5dfee3d70ebb472ed6",
+    "url": "/github.io/jonas-app.git/static/js/main.ac63d10f.chunk.js"
+  },
+  {
+    "revision": "e49c63bf9e8f32c75dd9",
+    "url": "/github.io/jonas-app.git/static/js/runtime-main.a23ff371.js"
+  },
+  {
+    "revision": "6af356ca9594e8ae904a31cc28ac96a5",
+    "url": "/github.io/jonas-app.git/static/media/1280.6af356ca.jpg"
+  },
+  {
+    "revision": "8905caa01950d5432a62a63221a9ad3c",
+    "url": "/github.io/jonas-app.git/static/media/1440.8905caa0.jpg"
+  },
+  {
+    "revision": "5028d72ff7d67f3d399a753e89a561ab",
+    "url": "/github.io/jonas-app.git/static/media/Cover.5028d72f.jpg"
+  },
+  {
+    "revision": "4b130f814b573a7f27652a60538ff66e",
+    "url": "/github.io/jonas-app.git/static/media/Wake.4b130f81.jpg"
+  },
+  {
+    "revision": "6eb9a348d100efba2f9d1599daf86836",
+    "url": "/github.io/jonas-app.git/static/media/bredare_nologo.6eb9a348.jpg"
+  },
+  {
+    "revision": "0e2febfe83167dba99cac5d15f58d537",
+    "url": "/github.io/jonas-app.git/static/media/cp.0e2febfe.jpg"
+  },
+  {
+    "revision": "e6a9841227622cd938377c1c3fc42ab9",
+    "url": "/github.io/jonas-app.git/static/media/nologo.e6a98412.jpg"
+  },
+  {
+    "revision": "a1973af41f3c845a18b1de145a0650fd",
+    "url": "/github.io/jonas-app.git/static/media/nologoleft.a1973af4.jpg"
+  },
+  {
+    "revision": "09dfe10025c84049cea2a1d1e4a1524f",
+    "url": "/github.io/jonas-app.git/static/media/webb_cover.09dfe100.jpg"
+  }
+]);
